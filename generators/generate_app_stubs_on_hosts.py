12e317ac8452b19b276aa9347007c8493815f946
Kaveh Shafiee <kaveh.shafiee@huawei.com>
2018-06-19 13:23:57 -0700

    'HelloWorld' example added as Template app.

--- generators/generate_app_stubs_on_hosts.py
+++ generators/generate_app_stubs_on_hosts.py
@@ -8,16 +8,15 @@
 
 import os, sys
 import subprocess
-from apps.todo import *
-#from apps.minnietwitter import *
+from apps.helloworld import *
 
 if __name__ == '__main__':
 
     sapphire_home = os.path.normpath(os.path.join(os.path.realpath(__file__), '../..'))
  
-    cp_app =  sapphire_home + '/sapphire/examples/' + app_name + '/build/libs/' + app_name + '.jar'
-    cp_sapphire = sapphire_home + '/sapphire/sapphire-core/build/libs/sapphire-core.jar'
-    cp_harmony = sapphire_home + '/sapphire/dependencies/apache.harmony/build/libs/apache.harmony.jar'
+    cp_app =  sapphire_home + '/sapphire/libs/' + app_name + '.jar'
+    cp_sapphire = sapphire_home + '/sapphire/libs/sapphire-core.jar'
+    cp_harmony = sapphire_home + '/sapphire/libs/apache.harmony.jar'
     
     cmd = ['java', '-cp',  cp_app + ':' + cp_sapphire + ':' + cp_harmony, 'sapphire.compiler.StubGenerator', sapphire_home + inFolder, package, sapphire_home + outFolder]
 
