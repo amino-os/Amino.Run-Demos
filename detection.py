46dc178337ede74e5873fad4395dcfb78d3ff11e
Dhawnitk <dhawnit.khurana@huawei.com>
2018-08-10 15:18:52 +0530

    Built JavaWrapper on top of egdeCV

--- detection.py
+++ detection.py
@@ -1,12 +1,12 @@
 import cv2
-
+import base64
 
 def face_detection():
     # Load a cascade file for detecting faces
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier('/home/root1/Desktop/EdgeCV/edgeCV/haarcascade_frontalface_default.xml')
 
     # Load an image
-    image = cv2.imread('images/obama.jpg', 1)
+    image = cv2.imread('/home/root1/Desktop/EdgeCV/edgeCV/images/obama.jpg', 1)
 
     # Convert to grayscale
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
@@ -18,5 +18,10 @@ def face_detection():
         cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
 
     frame = cv2.imencode('.jpg', image)[1].tobytes()
-    return (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    frame_serialize = base64.b64encode(frame).decode("utf-8")
+
+    # return (b'--frame\r\n'
+    #         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    print (frame_serialize)
+
+face_detection()
