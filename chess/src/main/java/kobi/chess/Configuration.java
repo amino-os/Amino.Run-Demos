e2e670439dcfd95cc279f6d9b83edf688a923832
Vishwa4jeet <vishwajeet@huawei.com>
2019-04-24 12:26:39 +0530

    updated port numbers

--- chess/src/main/java/kobi/chess/Configuration.java
+++ chess/src/main/java/kobi/chess/Configuration.java
@@ -4,6 +4,6 @@ public class Configuration {
 
     // TODO (Jeevan , 15/8/2018): Remove hardcoded IP addresses
 
-    public static String[] hostAddress ={"127.0.0.1","10000","10.0.2.15","10001"};
-    public static String[] kernelServerAddress={"127.0.0.1","10001","127.0.0.1","10002"};
+    public static String[] hostAddress ={"127.0.0.1","22346","10.0.2.15","22345"};
+    public static String[] kernelServerAddress={"127.0.0.1","22345","127.0.0.1","22344"};
 }
--- hanksTodo/src/main/java/amino/run/appexamples/hankstodo/glue/Configuration.java
+++ hanksTodo/src/main/java/amino/run/appexamples/hankstodo/glue/Configuration.java
@@ -1,5 +1,5 @@
 package amino.run.appexamples.hankstodo.glue;
 
 public class Configuration {
-    public static String [] hostAddress = { "127.0.0.1", "10000", "10.0.2.15", "10001" };
+    public static String [] hostAddress = { "127.0.0.1", "22346", "10.0.2.15", "22345" };
 }
